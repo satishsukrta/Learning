23909f450b4114d846a9b1f7e13ac183ddad8f84
Maria Jeysingh Anbu <sharihar@scouting.org>
2020-02-04 01:44:30 +0530

    Date Ex Complated

---
+++ date.js
@@ -0,0 +1,17 @@
+function date(date) {
+    var dt = new Date(date);
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var n = weekday[dt.getDay()];
+    console.log(n);
+    alert(n);
+}
+
+
+date("04/02/2019");
